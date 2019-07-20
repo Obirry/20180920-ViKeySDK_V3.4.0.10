63446d809948ecf7f2157225eb8752c1a214f0a3
WilsonDEGithub <297122675@qq.com>
2019-07-20 17:45:58 +0800

    new

---
+++ 4-[示例程序]/软件加密示例/Untiy3D/ViKeySample_Untiy3D/ViKeySample_Untiy3D/Assets/MainWnd.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices; // DllImport
+using ViKeySample;
+
+
+public class MainWnd : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Program.ViKeyTest();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+    }
+}
